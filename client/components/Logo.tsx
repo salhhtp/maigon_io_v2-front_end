22076725cc395284e62b29e7ee5e787a01adfe4f
salhhtp <salh_htp@icloud.com>
2025-12-04 01:04:46 +0100

    Logo update

--- client/components/Logo.tsx
+++ client/components/Logo.tsx
@@ -8,10 +8,10 @@ interface LogoProps {
 
 const Logo: React.FC<LogoProps> = ({ className = '', size = 'lg', align = 'center' }) => {
   const sizeClasses = {
-    sm: 'text-base',
-    md: 'text-lg',
-    lg: 'text-xl',
-    xl: 'text-2xl'
+    sm: 'h-6',
+    md: 'h-8',
+    lg: 'h-10',
+    xl: 'h-12',
   };
 
   const alignClasses = {
